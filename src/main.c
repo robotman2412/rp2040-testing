0b8aeeef19498d6dc1c24f7fd8bfa379ed9298cf
RobotMan2412 <julian@scheffers.net>
2023-02-11 14:23:07 +0100

    Fix the demo real quick

--- src/main.c
+++ src/main.c
@@ -4,9 +4,19 @@
 #include <stdlib.h>
 #include "elfloader.h"
 #include "abi_impl.h"
+// #include "ili9341.h"
+// #include <hardware/spi.h>
 
 extern const unsigned char elf_file[];
 extern const unsigned int elf_file_length;
+
+#define SPI_MOSI    19
+#define SPI_MISO    16 // (unused)
+#define SPI_SCLK    18
+
+#define DISP_RST 13
+#define DISP_CS  14
+#define DISP_DCX 15 // (same as SPI_DC for display)
 
 bool match_ptrtab(void **entry) {
 	if ((size_t) *entry < 0x20030000 || (size_t) *entry > 0x2003efff) {
@@ -79,9 +89,28 @@ int main() {
 	printf("\n\n\n\n\n\n\n\n\n\n\n\nStartup time!\n\n");
 	sleep_ms(500);
 	
+	// size_t buf_len = 320 * 240;
+	// size_t buf_size = 2 * buf_len;
+	// uint16_t *buf = malloc(buf_size);
+	// memset(buf, 0, buf_size);
+	
+	// spi_init(spi0, 100000);
+	
+	// ILI9341 disp = {
+	// 	.spi_bus    = 0,
+	// 	.pin_cs     = DISP_CS,
+	// 	.pin_dcx    = DISP_DCX,
+	// 	.pin_reset  = DISP_RST,
+	// 	.rotation   = 1,
+	// 	.color_mode = true,
+	// 	.callback   = NULL,
+	// };
+	// ili9341_init(&disp);
+	// ili9341_write(&disp, buf);
+	
 	// Interpret ELF file.
-	// FILE *elf_fd = fmemopen((void *) elf_file, elf_file_length, "r");
-	FILE *elf_fd = input_test();
+	FILE *elf_fd = fmemopen((void *) elf_file, elf_file_length, "r");
+	// FILE *elf_fd = input_test();
 	elf_ctx_t ctx = elf_interpret(elf_fd, false);
 	if (ctx.valid) {
 		printf("ELF file valid.\n");
@@ -108,7 +137,7 @@ int main() {
 				sleep_ms(50);
 				printf("Running.\n");
 				
-				int (*entryFunc)() = ctx.entrypoint;
+				int (*entryFunc)() = (const void *) ctx.entrypoint;
 				int res = entryFunc();
 				printf("Exit code %d (0x%x)\n", res, res);
 			} else {
